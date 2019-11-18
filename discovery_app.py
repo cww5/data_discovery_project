4a57448347fe2e93dc6365b9937bafae11669b82
Connor Watson <connorwatson0811@gmail.com>
2019-11-17 22:46:06 -0500

    11-17-19 update app appearance

--- discovery_app.py
+++ discovery_app.py
@@ -19,17 +19,23 @@ from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 data_path = 'C:\\Users\\watson\\Documents\\GitHub\\data_discovery_project\\datasets\\'
-data_file = data_path + 'yearly_data.csv'
+num_file = data_path + 'yearly_numeric_data.csv'
 stan_file = data_path + 'yearly_stan_data.csv'
-df = pd.read_csv(stan_file, index_col=0)
+num_df = pd.read_csv(num_file, index_col=0)
+stan_df = pd.read_csv(stan_file, index_col=0)
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df_columns = list(df.columns)
+# df_columns apply for both num_df and stan_df
+df_columns = list(stan_df.columns)
 df_columns.remove('year')
 drop_options = [{'value':col, 'label':' '.join(col.lower().split('_'))} for col in df_columns]
 
+full_df = num_df[num_df.columns[num_df.notnull().all()]]
+years = sorted(full_df['year'].values)
 
-def make_graph(graph_id, opts):
+print(type(years[0]))
+print(years)
+def make_graph(graph_id, opts, df):
     return dcc.Graph(
         id=graph_id,
         figure={
@@ -43,80 +49,131 @@ def make_graph(graph_id, opts):
     )
 
 app.layout = html.Div(children=[
-    html.H1(children='Data Discovery'),
-    html.H3(children='Finding Relationships Amongst Disparate Data Sets'),
-    html.Div(children='By: Connor Watson, Priyanka Racharla, and Keval Kavle'),
+    html.Div(children=[
+        html.H1(children='Data Discovery'),
+        html.H3(children='Finding Relationships Amongst Disparate Data Sets'),
+        html.Div(children='By: Connor Watson, Priyanka Racharla, and Keval Kavle'),
 
-    html.Div(children='''
-    In this application we seek to find relationships amongst data sets
-    which seemingly have no relationship. Thanks to recent efforts, we have
-    access to lots of New York City data, and we intend to use data-driven story
-    telling to inform the user of yearly trends.
-    '''),
-    html.Br(),
+        html.Div(children='''
+        In this application we seek to find relationships amongst data sets
+        which seemingly have no relationship. Thanks to recent efforts, we have
+        access to lots of New York City data, and we intend to use data-driven story
+        telling to inform the user of yearly trends.
+        ''', style={'text-align':'left'}),
+        html.Br(),
 
-    html.Div(children='''
-    The growth of the human population seems to keep increasing as time
-    goes on. As the Internet has boomed, NYC has become more of a hub
-    for both jobs and technology. With the increase in population comes
-    an increase in effect on the environment as well. People joke about
-    the 'dirty Hudson River', but in actuality, the condition of NYC and
-    our Earth is no joke.
-    '''),
-    html.Br(),
-    
-    html.Div(children='''
-    Within our findings, we can see that NYC's population has very clearly
-    been increasing. Using the tool below, you can discover this trend
-    yourself. It may also be interesting to compare the constant increase
-    in population with the trends in other attributes of our data set
-    as well.
-    '''),
-    html.Br(),
+        html.Div(children='''
+        The growth of the human population seems to keep increasing as time
+        goes on. As the Internet has boomed, NYC has become more of a hub
+        for both jobs and technology. With the increase in population comes
+        an increase in effect on the environment as well. People joke about
+        the 'dirty Hudson River', but in actuality, the condition of NYC and
+        our Earth is no joke.
+        ''', style={'text-align':'left'}),
+        html.Br(),
+        
+        html.Div(children='''
+        Within our findings, we can see that NYC's population has very clearly
+        been increasing. Using the tool below, you can discover this trend
+        yourself. It may also be interesting to compare the constant increase
+        in population with the trends in other attributes of our data set
+        as well.
+        ''', style={'text-align':'left'}),
+        html.Br(),
 
-    html.Div(children='''
-    One important discovery is that even though more and more people have
-    populated NYC, it seems there is an inverse effect on the amount of
-    water consumed per year in millions of gallons per day. After 1990,
-    it seems as though there has been a general decline in the amount
-    of water consumed per day. Could this be thanks to the realization
-    that we are making a negative impact on the environment? If you select
-    the 'schoolorganictons' attribute, you can clearly see that more tons
-    of organic waste has been collected from schools, pointing to the
-    acknowledgement by the Department of Education. What else can you discover?
-    '''),
-    html.Br(),
+        html.Div(children='''
+        One important discovery is that even though more and more people have
+        populated NYC, it seems there is an inverse effect on the amount of
+        water consumed per year in millions of gallons per day. After 1990,
+        it seems as though there has been a general decline in the amount
+        of water consumed per day. Could this be thanks to the realization
+        that we are making a negative impact on the environment? If you select
+        the 'schoolorganictons' attribute, you can clearly see that more tons
+        of organic waste has been collected from schools, pointing to the
+        acknowledgement by the Department of Education. What else can you discover?
+        ''', style={'text-align':'left'}),
+        html.Br(),
 
-    html.Div('''
-    NOTE: This graph is NOT to scale. All data points have been standardized
-    to fall in the range [0,1]. This graph allows you to see the overall trends.
-    '''),
-    html.Br(),
-    
-    dcc.Dropdown(
-        id='dropdown-options',
-        options=drop_options,
-        value=['new_york_city_population',
-                'nyc_consumption_million_gallons_per_day',
-                #'number_of_indoor_complaints',
-                #'REFUSETONSCOLLECTED',
-                #'PAPERTONSCOLLECTED',
-                #'RESORGANICSTONS',
-                #'SCHOOLORGANICTONS'
-                ],
-        multi=True
-    ),
-    html.Div(id='graph-1')
-])
+        html.Div('''
+        NOTE: This graph is NOT to scale. All data points have been standardized
+        to fall in the range [0,1]. This graph allows you to see the overall trends.
+        ''', style={'text-align':'left'}),
+        html.Br(),
+        
+        dcc.Dropdown(
+            id='dropdown-options',
+            options=drop_options,
+            value=['new_york_city_population',
+                    'nyc_consumption_million_gallons_per_day',
+                    #'number_of_indoor_complaints',
+                    #'REFUSETONSCOLLECTED',
+                    #'PAPERTONSCOLLECTED',
+                    #'RESORGANICSTONS',
+                    #'SCHOOLORGANICTONS'
+                    ],
+            multi=True
+        ),
+        html.Div(id='graph-1'),
+
+        html.Br(),
+
+        html.Div(id='min-year', children='', style={'display':'none'}),
+        html.Div(id='max-year', children='', style={'display':'none'}),
+        
+        html.Div(
+            html.Div(
+                dcc.RangeSlider(
+                    id='year-slider',
+                    min=1979, max=2018, step=1,
+                    value=[1979,2018],
+                    marks={y:str(y) for y in range(1979,2020,5)}
+                ),
+                style={'width': '600px', 'display': 'inline-block'}
+            ),
+            style={'text-align': 'center'}
+        ),
+        html.Br(),
+        html.Br(),
+        
+        dcc.Dropdown(
+            id='select-y',
+            options=drop_options,
+            value=['new_york_city_population'],
+            multi=False
+        ),
+        dcc.Dropdown(
+            id='select-x',
+            options=drop_options,
+            value=['nyc_consumption_million_gallons_per_day'],
+            multi=False
+        ),
+        html.Div(id='graph-2'),
+    ], style={'width': '1000px', 'display': 'inline-block'})
+], style={'text-align': 'center'})
 
 @app.callback(
-    Output("graph-1", "children"),
-    [Input("dropdown-options", "value")],
+    Output('graph-1', 'children'),
+    [Input('dropdown-options', 'value')],
 )
 def update_options(options_selected):
     #options_selected is the list of dropdown options
-    my_graph = make_graph('yearly-data', options_selected)
+    my_graph = make_graph('yearly-data', options_selected, stan_df)
     return my_graph
+
+@app.callback(
+    Output('graph-2', 'children'),
+    [Input('year-slider', 'value')])
+def update_years_output(value):
+    return 'You have selected "{}"'.format(value)
+
+@app.callback(
+    [Output('min-year', 'children'),
+     Output('max-year', 'children')],
+    [Input('select-x', 'value'),
+     Input('select-y', 'value')]
+    )
+def update_graph_2(x_label, y_label):
+    return pass
 
 
 def main():
